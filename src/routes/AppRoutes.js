7d2a2a245ce648d1fa645f6f5688b163cbd54b6e
Prachi Kashyap <pk23@pdx.edu>
2022-11-30 01:16:35 -0800

    pull from main

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "../pages/About";
 import Home from "../pages/Home";
 import Contact from "../pages/Contact";
+//import HowItWorks from "../pages/import HowItWorks from "../pages/Contact";
 
 function AppRoutes() {
   return (
